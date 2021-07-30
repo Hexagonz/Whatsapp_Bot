78440dd283aed4089282e0a11dbe386e353501a8
I am Manurios <87898962+ManuriosX@users.noreply.github.com>
2021-07-30 16:29:40 +0800

    Update ind.ts

--- lib/lang/ind.ts
+++ lib/lang/ind.ts
@@ -7,6 +7,12 @@ export const IndTunggu = () => {
 export const IndBukanVid = () => {
 	return `Maaf file yang anda dikirim bukan termasuk video`
 }
+export const IndBukanAud = () => {
+	return `Maaf file yang anda dikirim bukan termasuk audio`
+}
 export const IndToVid = () => {
 	return `Maaf Fitur media tovideo anda sedang error harap ganti media`
+}
+export const IndToCute = () => {
+	return `Maaf Fitur media tocute anda sedang error harap coba lagi`
 }
